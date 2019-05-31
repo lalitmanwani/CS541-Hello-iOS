1de6d61aafb6893836ded3355dc4f8f2390e96fd
lalitmanwani <lalit631995@gmail.com>
2019-05-30 23:57:50 -0400

    First Attempt

---
+++ CS541-Hello-iOS/CS541-Hello-iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CS541-Hello-iOS
+//
+//  Created by Saket Chinchalikar on 5/30/19.
+//  Copyright © 2019 Lalit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CS541-Hello-iOS/CS541-Hello-iOS/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  CS541-Hello-iOS
+//
+//  Created by Saket Chinchalikar on 5/30/19.
+//  Copyright © 2019 Lalit. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
