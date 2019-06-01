86e667f72f7ea73d619a9cdbcd8a8c1719d7afa1
lalitmanwani <lalit631995@gmail.com>
2019-05-31 22:29:23 -0400

    Update 1.1
    A whole new UI design.
    And Now the app has six different languages to say hello In when the button is pressed.

--- CS541-Hello-iOS/CS541-Hello-iOS/ViewController.m
+++ CS541-Hello-iOS/CS541-Hello-iOS/ViewController.m
@@ -9,15 +9,55 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *nameImage;
 
 @end
+void changefunc(void);
 
 @implementation ViewController
+NSString *nameimg[] = {
+    @"English",
+    @"Hindi",
+    @"Dutch",
+    @"French",
+    @"Chinese",
+    @"Arabic"
+};
+int x = 0;
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+      [self changefunc];
 }
+- (IBAction)imagechanger:(id)sender {
+    [self changefunc];
+    
+}
+
+
+
+- (void)changefunc{
+    
+    srand(0);
+    x = arc4random() % 6;
+    
+    _nameImage.image = [UIImage imageNamed: nameimg[x]];
+    printf("%d",x);
+    
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 @end
